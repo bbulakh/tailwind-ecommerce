79ea96c4ec58ddda58137a82d95dbf72cfbf23fc
Sergey Romanenko <awilum@msn.com>
2023-03-28 23:12:37 +0800

    improve build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const { src, dest, task, series } = require('gulp');
 const exec = require('child_process').exec;
-
+const htmlreplace = require('gulp-html-replace');
+const rename = require('gulp-rename');
+const md5File = require('md5-file');
 
 /**
  * Task: tailwind 
@@ -14,6 +16,30 @@ task('tailwind', function (cb) {
   });
 });
 
+task('build-pages', function() {
+  return src('./src/pages/*.html')
+    .pipe(dest('./public/'));
+});
+
+task('build-css-version', function() {
+  const hash = md5File.sync('./public/assets/dist/css/tailwind-ecommerce.css');
+  return src('./public/assets/dist/css/tailwind-ecommerce.css')
+      .pipe(rename(`tailwind-ecommerce-${hash}.css`))
+      .pipe(dest('./public/assets/dist/css/'));
+});
+
+task('build-html-updates', function() {
+  const hash = md5File.sync('./public/assets/dist/css/tailwind-ecommerce.css');
+  return src('./public/*.html')
+    .pipe(htmlreplace({
+        'css': `/assets/dist/css/tailwind-ecommerce-${hash}.css`
+    }))
+    .pipe(dest('public/'));
+});
+
 task('default', series(
-  'tailwind'
+  'tailwind',
+  'build-pages',
+  'build-css-version',
+  'build-html-updates'
 ));
