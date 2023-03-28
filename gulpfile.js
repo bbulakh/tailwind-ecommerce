2cd7d79da1c2bbeb03a479cddc573a695cbff096
Bogdan Bulakh <95144713+bbulakh@users.noreply.github.com>
2023-03-28 18:34:43 +0300

    add image build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
-const { src, dest, task, series } = require('gulp');
-const exec = require('child_process').exec;
-const htmlreplace = require('gulp-html-replace');
-const rename = require('gulp-rename');
-const md5File = require('md5-file');
+const { src, dest, task, series } = require("gulp");
+const exec = require("child_process").exec;
+const htmlreplace = require("gulp-html-replace");
+const rename = require("gulp-rename");
+const md5File = require("md5-file");
 
 /**
- * Task: tailwind 
+ * Task: tailwind
  */
-task('tailwind', function (cb) {
-  const command = 'npx tailwindcss -i ./src/assets/src/css/tailwind-ecommerce.css -o ./public/assets/dist/css/tailwind-ecommerce.css';
+task("tailwind", function (cb) {
+  const command =
+    "npx tailwindcss -i ./src/assets/src/css/tailwind-ecommerce.css -o ./public/assets/dist/css/tailwind-ecommerce.css";
   exec(command, function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
@@ -16,30 +17,40 @@ task('tailwind', function (cb) {
   });
 });
 
-task('build-pages', function() {
-  return src('./src/pages/*.html')
-    .pipe(dest('./public/'));
+task("build-pages", function () {
+  return src("./src/pages/*.html").pipe(dest("./public/"));
 });
 
-task('build-css-version', function() {
-  const hash = md5File.sync('./public/assets/dist/css/tailwind-ecommerce.css');
-  return src('./public/assets/dist/css/tailwind-ecommerce.css')
-      .pipe(rename(`tailwind-ecommerce-${hash}.css`))
-      .pipe(dest('./public/assets/dist/css/'));
+task('build-images', function() {
+  return src('./src/assets/images/*.{png,svg,ico}')
+    .pipe(dest('./public/assets/images/'));
 });
 
-task('build-html-updates', function() {
-  const hash = md5File.sync('./public/assets/dist/css/tailwind-ecommerce.css');
-  return src('./public/*.html')
-    .pipe(htmlreplace({
-        'css': `/assets/dist/css/tailwind-ecommerce-${hash}.css`
-    }))
-    .pipe(dest('public/'));
+task("build-css-version", function () {
+  const hash = md5File.sync("./public/assets/dist/css/tailwind-ecommerce.css");
+  return src("./public/assets/dist/css/tailwind-ecommerce.css")
+    .pipe(rename(`tailwind-ecommerce-${hash}.css`))
+    .pipe(dest("./public/assets/dist/css/"));
 });
 
-task('default', series(
-  'tailwind',
-  'build-pages',
-  'build-css-version',
-  'build-html-updates'
-));
+task("build-html-updates", function () {
+  const hash = md5File.sync("./public/assets/dist/css/tailwind-ecommerce.css");
+  return src("./public/*.html")
+    .pipe(
+      htmlreplace({
+        css: `/assets/dist/css/tailwind-ecommerce-${hash}.css`,
+      })
+    )
+    .pipe(dest("public/"));
+});
+
+task(
+  "default",
+  series(
+    "tailwind",
+    "build-pages",
+    "build-images",
+    "build-css-version",
+    "build-html-updates"
+  )
+);
